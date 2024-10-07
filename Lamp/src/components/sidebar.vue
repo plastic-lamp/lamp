739c7a9e93d334871b7c65e1c1c5175f6a955d11
GreenestGoat <fionnanloughlin@gmail.com>
2024-10-07 14:34:19 +0100

    update

--- Lamp/src/components/sidebar.vue
+++ Lamp/src/components/sidebar.vue
@@ -2,7 +2,7 @@
     <div class="sidebar" :class="{ expanded }">
         <div class="sidebar-logo">
             <RouterLink to="/" tabindex="-1">
-                <button class="home-button">
+                <button class="home-button" v-tippy="{ content: 'Lamp' }">
                     <span class="sidebar-icon">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-lamp"><path d="M8 2h8l4 10H4L8 2Z"/><path d="M12 12v6"/><path d="M8 22v-2c0-1.1.9-2 2-2h4a2 2 0 0 1 2 2v2H8Z"/></svg>
                     </span>
@@ -12,7 +12,7 @@
         </div>
         <div class="sidebar-top">
             <RouterLink to="/icons" tabindex="-1">
-                <button class="icons-button">
+                <button class="icons-button" v-tippy="{ content: 'Icons' }">
                     <span class="sidebar-icon">
                         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-icons" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.7" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6.5 6.5m-3.5 0a3.5 3.5 0 1 0 7 0a3.5 3.5 0 1 0 -7 0"/><path d="M2.5 21h8l-4 -7z"/><path d="M14 3l7 7"/><path d="M14 10l7 -7"/><path d="M14 14h7v7h-7z"/>
@@ -22,7 +22,7 @@
                 </button>
             </RouterLink>
             <RouterLink to="/typography" tabindex="-1">
-                <button class="typography-button">
+                <button class="typography-button" v-tippy="{ content: 'Typography' }">
                     <span class="sidebar-icon">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-type"><polyline points="4 7 4 4 20 4 20 7"/><line x1="9" x2="15" y1="20" y2="20"/><line x1="12" x2="12" y1="4" y2="20"/></svg>
                     </span>
@@ -30,7 +30,7 @@
                 </button>
             </RouterLink>
             <RouterLink to="/3d" tabindex="-1">
-                <button class="three-dimensional-button">
+                <button class="three-dimensional-button" v-tippy="{ content: '3D' }">
                     <span class="sidebar-icon">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-box"><path d="M21 8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16Z"/><path d="m3.3 7 8.7 5 8.7-5"/><path d="M12 22V12"/></svg>
                     </span>
@@ -38,7 +38,7 @@
                 </button>
             </RouterLink>
             <RouterLink to="/libraries" tabindex="-1">
-                <button class="libraries-button">
+                <button class="libraries-button" v-tippy="{ content: 'Libraries' }">
                     <span class="sidebar-icon">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-library"><path d="m16 6 4 14"/><path d="M12 6v14"/><path d="M8 8v12"/><path d="M4 4v16"/></svg>
                     </span>
@@ -46,7 +46,7 @@
                 </button>
             </RouterLink>
             <RouterLink to="/components" tabindex="-1">
-                <button class="components-button">
+                <button class="components-button" v-tippy="{ content: 'Components' }">
                     <span class="sidebar-icon">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-component"><path d="M5.5 8.5 9 12l-3.5 3.5L2 12l3.5-3.5Z"/><path d="m12 2 3.5 3.5L12 9 8.5 5.5 12 2Z"/><path d="M18.5 8.5 22 12l-3.5 3.5L15 12l3.5-3.5Z"/><path d="m12 15 3.5 3.5L12 22l-3.5-3.5L12 15Z"/></svg>
                     </span>
@@ -54,7 +54,7 @@
                 </button>
             </RouterLink>
             <a href="https://ko-fi.com/brick_wall" target="_blank" tabindex="-1">
-                <button class="support-button">
+                <button class="support-button" v-tippy="{ content: 'Support' }">
                     <span class="sidebar-icon">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-coffee"><path d="M10 2v2"/><path d="M14 2v2"/><path d="M16 8a1 1 0 0 1 1 1v8a4 4 0 0 1-4 4H7a4 4 0 0 1-4-4V9a1 1 0 0 1 1-1h14a4 4 0 1 1 0 8h-1"/><path d="M6 2v2"/></svg>
                     </span>
@@ -65,7 +65,7 @@
         <div class="sidebar-gap"></div>
         <div class="sidebar-bottom">
             <a class="open-button-container" tabindex="-1" @click="toggleSidebar">
-                <button class="open-button">
+                <button class="open-button" v-tippy="{ content: 'Open' }">
                     <span class="sidebar-icon">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-panel-right"><rect width="18" height="18" x="3" y="3" rx="2"/><path d="M15 3v18"/></svg>
                     </span>
@@ -73,7 +73,7 @@
                 </button>
             </a>
             <a class="close-button-container" tabindex="-1" @click="toggleSidebar">
-                <button class="close-button">
+                <button class="close-button" v-tippy="{ content: 'Close' }">
                     <span class="sidebar-icon">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-panel-right-open"><rect width="18" height="18" x="3" y="3" rx="2"/><path d="M15 3v18"/><path d="m10 15-3-3 3-3"/></svg>
                     </span>
@@ -81,7 +81,7 @@
                 </button>
             </a>
             <a tabindex="-1">
-                <button class="theme-button">
+                <button class="theme-button" v-tippy="{ content: 'Theme' }">
                     <span class="sidebar-icon">
                         <svg class="dark-mode-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"/></svg>
                         <svg class="light-mode-icon hidden" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="4"/><path d="M12 2v2"/><path d="M12 20v2"/><path d="m4.93 4.93 1.41 1.41"/><path d="m17.66 17.66 1.41 1.41"/><path d="M2 12h2"/><path d="M20 12h2"/><path d="m6.34 17.66-1.41 1.41"/><path d="m19.07 4.93-1.41 1.41"/></svg>
@@ -94,18 +94,23 @@
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            expanded: false,
-        };
-    },
-    methods: {
-        toggleSidebar() {
-            this.expanded = !this.expanded;
+    import { directive } from 'vue-tippy'
+
+    export default {
+        directives: {
+            tippy: directive,
         },
-    },
-};
+        data() {
+            return {
+                expanded: false,
+            };
+        },
+        methods: {
+            toggleSidebar() {
+                this.expanded = !this.expanded;
+            },
+        },
+    };
 </script>
 
 <style>
@@ -337,6 +342,15 @@ export default {
 .sidebar .sidebar-bottom a.router-link-exact-active button:active {
     background-color: #27272A;
     color: rgba(250, 250, 250, 1);
+}
+
+.tippy-box {
+  background-color: #09090B;
+  color: rgba(250, 250, 250, 1);
+  border-radius: 6px;
+  border: 1px solid #27272A;
+  margin-left: 10px;
+  font-family: "Open Sans", sans-serif;
 }
 </style>
 
