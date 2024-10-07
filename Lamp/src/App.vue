6c58281c8d217067c7b9ee59924dab76cac18972
GreenestGoat <fionnanloughlin@gmail.com>
2024-10-07 12:48:23 +0100

    update

--- Lamp/src/App.vue
+++ Lamp/src/App.vue
@@ -4,18 +4,7 @@ import sidebar from './components/sidebar.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <sidebar />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <sidebar />
 
   <RouterView />
 </template>
--- Lamp/src/components/sidebar.vue
+++ Lamp/src/components/sidebar.vue
@@ -53,14 +53,14 @@
                     <span class="sidebar-title">Components</span>
                 </button>
             </RouterLink>
-            <RouterLink to="https://ko-fi.com/brick_wall" tabindex="-1">
+            <a href="https://ko-fi.com/brick_wall" target="_blank" tabindex="-1">
                 <button class="support-button">
                     <span class="sidebar-icon">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-coffee"><path d="M10 2v2"/><path d="M14 2v2"/><path d="M16 8a1 1 0 0 1 1 1v8a4 4 0 0 1-4 4H7a4 4 0 0 1-4-4V9a1 1 0 0 1 1-1h14a4 4 0 1 1 0 8h-1"/><path d="M6 2v2"/></svg>
                     </span>
                     <span class="sidebar-title">Support</span>
                 </button>
-            </RouterLink>
+            </a>
         </div>
         <div class="sidebar-gap"></div>
         <div class="sidebar-bottom">
