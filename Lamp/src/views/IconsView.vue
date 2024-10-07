6ebe22ce9d9528193cbec50f3ad7615dfa5d20b1
GreenestGoat <fionnanloughlin@gmail.com>
2024-10-07 16:40:38 +0100

    added cards not finished needs to be connected to json

--- Lamp/src/views/IconsView.vue
+++ Lamp/src/views/IconsView.vue
@@ -1,5 +1,52 @@
 <template>
   <div class="main">
-    <h1>Icons</h1>
+    <div class="card-container">
+      <div class="card-row">
+        <a href="https://simpleicons.org/" target="_blank" class="card">
+          <img src="https://res.cloudinary.com/cosmocloudinary/image/upload/v1705765217/freesets/icons/jvhe6jrpy38zrqi6dhvp.webp" alt="">
+          <button class="card-btn" tabindex="-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-right"><path d="M7 7h10v10"/><path d="M7 17 17 7"/></svg>
+          </button>
+          <span class="card-title">Simple Icons</span>
+        </a>
+        <a href="https://www.radix-ui.com/icons" target="_blank" class="card">
+          <img src="https://res.cloudinary.com/cosmocloudinary/image/upload/v1705772255/freesets/icons/mzzgjxfoem5kafvfyyhh.webp" alt="">
+          <button class="card-btn" tabindex="-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-right"><path d="M7 7h10v10"/><path d="M7 17 17 7"/></svg>
+          </button>
+          <span class="card-title">Radix Icons</span>
+        </a>
+        <a href="https://www.navbar.gallery/" target="_blank" class="card">
+          <img src="https://res.cloudinary.com/cosmocloudinary/image/upload/freesets/inspirations/navbargallery" alt="">
+          <button class="card-btn" tabindex="-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-right"><path d="M7 7h10v10"/><path d="M7 17 17 7"/></svg>
+          </button>
+          <span class="card-title">Navbar Gallery</span>
+        </a>
+      </div>
+      <div class="card-row">
+        <a href="https://www.navbar.gallery/" target="_blank" class="card">
+          <img src="https://res.cloudinary.com/cosmocloudinary/image/upload/freesets/inspirations/navbargallery" alt="">
+          <button class="card-btn" tabindex="-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-right"><path d="M7 7h10v10"/><path d="M7 17 17 7"/></svg>
+          </button>
+          <span class="card-title">Navbar Gallery</span>
+        </a>
+        <a href="https://www.navbar.gallery/" target="_blank" class="card">
+          <img src="https://res.cloudinary.com/cosmocloudinary/image/upload/freesets/inspirations/designspells" alt="">
+          <button class="card-btn" tabindex="-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-right"><path d="M7 7h10v10"/><path d="M7 17 17 7"/></svg>
+          </button>
+          <span class="card-title">Navbar Gallery</span>
+        </a>
+        <a href="https://www.navbar.gallery/" target="_blank" class="card">
+          <img src="https://res.cloudinary.com/cosmocloudinary/image/upload/freesets/inspirations/navbargallery" alt="">
+          <button class="card-btn" tabindex="-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-right"><path d="M7 7h10v10"/><path d="M7 17 17 7"/></svg>
+          </button>
+          <span class="card-title">Navbar Gallery</span>
+        </a>
+      </div>
+    </div>
   </div>
 </template>
