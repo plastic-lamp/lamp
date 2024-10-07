85f32748a54bee5ec017aa62b6bc184b98b234a1
GreenestGoat <fionnanloughlin@gmail.com>
2024-10-07 22:14:13 +0100

    added lazy loading

--- Lamp/src/views/TypographyView.vue
+++ Lamp/src/views/TypographyView.vue
@@ -4,7 +4,7 @@
       <div v-for="(row, rowIndex) in cardRows" :key="rowIndex" class="card-row">
         <template v-for="(icon, index) in row" :key="icon ? icon.id : `empty-${index}`">
           <a v-if="icon" :href="icon.url" target="_blank" class="card">
-            <img :src="icon.image_url" :alt="icon.title">
+            <img :src="icon.image_url" :alt="icon.title" loading="lazy">
             <button class="card-btn" tabindex="-1">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-right"><path d="M7 7h10v10"/><path d="M7 17 17 7"/></svg>
             </button>
