95691ce04ad5554c609a73208304fdc26222f0c5
GreenestGoat <fionnanloughlin@gmail.com>
2024-10-07 16:43:16 +0100

    Update IconsView.vue

--- Lamp/src/views/IconsView.vue
+++ Lamp/src/views/IconsView.vue
@@ -16,12 +16,12 @@
           </button>
           <span class="card-title">Radix Icons</span>
         </a>
-        <a href="https://www.navbar.gallery/" target="_blank" class="card">
-          <img src="https://res.cloudinary.com/cosmocloudinary/image/upload/freesets/inspirations/navbargallery" alt="">
+        <a href="https://svgl.vercel.app/" target="_blank" class="card">
+          <img src="https://res.cloudinary.com/cosmocloudinary/image/upload/freesets/icons/svgl" alt="">
           <button class="card-btn" tabindex="-1">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-right"><path d="M7 7h10v10"/><path d="M7 17 17 7"/></svg>
           </button>
-          <span class="card-title">Navbar Gallery</span>
+          <span class="card-title">svgl</span>
         </a>
       </div>
       <div class="card-row">
