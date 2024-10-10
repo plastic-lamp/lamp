97d546e709461d1e9880b636deae5f672259dd9e
Brick_Wall <118539051+GreenestGoat@users.noreply.github.com>
2024-10-10 14:20:57 +0100

    Update IconsView.vue

--- Lamp/src/views/IconsView.vue
+++ Lamp/src/views/IconsView.vue
@@ -5,7 +5,7 @@
         <template v-for="(icon, index) in row" :key="icon ? icon.id : `empty-${index}`">
           <a v-if="icon" :href="icon.url" target="_blank" class="card">
             <img :src="icon.image_url" :alt="icon.title" loading="lazy">
-            <button class="card-btn" tabindex="-1">
+            <button aria-label="Open in new tab" title="Open in new tab" class="card-btn" tabindex="-1">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-right"><path d="M7 7h10v10"/><path d="M7 17 17 7"/></svg>
             </button>
             <span class="card-title">{{ icon.title }}</span>
